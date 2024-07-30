ec3177fcc0371515ec450cca914fb380db426ec2
Horus Alkebu-Lan <horusjr@me.com>
2024-07-30 11:14:56 -0700

    added root attack

---
+++ linkedin/can_rook_attack.py
@@ -0,0 +1,36 @@
+def rooks_are_safe(chessboard):
+    # idea: basically all columns and row must be 0 or 1
+    square_size = len(chessboard[0])
+
+    # first row by row iterating across columns
+    for i in range(square_size):
+        row_sum = 0
+        for j in range(square_size):
+            row_sum += chessboard[i][j]
+        if row_sum != 0 and row_sum != 1:
+            return False
+        
+    # next column by column, iterating across rows
+    for j in range(square_size):
+        col_sum = 0
+        for i in range(square_size):
+            col_sum += chessboard[i][j]
+        if col_sum != 0 and col_sum != 1:
+            return False
+        
+    return True
+
+
+if __name__ == "__main__":
+
+    chessboard = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]]
+    result = rooks_are_safe(chessboard)
+    expected = True
+    print(f"chessboard: {chessboard}, result: {result}, expected: {expected}")
+    assert result == expected
+
+    chessboard = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1]]
+    result = rooks_are_safe(chessboard)
+    expected = False
+    print(f"chessboard: {chessboard}, result: {result}, expected: {expected}")
+    assert result == expected
