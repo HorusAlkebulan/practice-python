2f267c5c0480fe9b841f973b19cb7220f0d2dfd0
Horus Alkebu-Lan <horusjr@me.com>
2024-08-11 14:36:58 -0700

    finished factorial, next ch 3

--- linkedin/python_recursion/factorial_recursive.py
+++ linkedin/python_recursion/factorial_recursive.py
@@ -7,6 +7,9 @@ Robin Andrews - https://compucademy.net/
 # pytest factorial_recursive.py -v
 
 
+from trace_recursion import trace
+
+
 def factorial_recursive_while(n):  # Condition-controlled version
 
     # n = 5
@@ -33,6 +36,7 @@ def factorial_recursive(n):
 
     # base case to exit
     if n <= 1:
+        # what do we return in the base case?
         return 1
 
     # take action
@@ -71,3 +75,10 @@ def test_50():
         factorial_recursive_while(50)
         == 30414093201713378043612608166064768844377641568960512000000000000
     )
+
+if __name__ == "__main__":
+
+    # trace this function
+    factorial = trace(factorial_recursive_while)
+    factorial_recursive_while(5)
+    # print(f"x: {x} for n=5")
