7e63d3440625ac5fe1234cd66740480a0b7c1bc9
Horus Alkebu-Lan <horusjr@me.com>
2024-08-21 12:20:31 -0700

    use and instead of *

--- linkedin/foundations_algo_thinking/fizz_buzz.py
+++ linkedin/foundations_algo_thinking/fizz_buzz.py
@@ -4,20 +4,21 @@ def fizz_buzz(max_output_size):
 
     # every 3rd say fizz
     # every 5th say buzz
-    # if 15th, say fizz buzz
+    # if both true, say fizz buzz
     # otherwise, say the integer
     interval_fizz = 3
     interval_buzz = 5
-    interval_fizz_buzz = 15
     output = []
 
     for i in range(max_output_size):
         base_one_idx = i + 1
-        if base_one_idx % interval_fizz_buzz == 0:
+        fizz_mod = base_one_idx % interval_fizz
+        buzz_mod = base_one_idx % interval_buzz
+        if fizz_mod == 0 and buzz_mod == 0:
             output.append("fizz buzz")
-        elif base_one_idx % interval_fizz == 0:
+        elif fizz_mod == 0:
             output.append("fizz")
-        elif base_one_idx % interval_buzz == 0:
+        elif buzz_mod == 0:
             output.append("buzz")
         else:
             output.append(str(base_one_idx))
