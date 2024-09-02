e0426838409a9e50ad5855e05d49b05a5eedbce1
Horus Alkebu-Lan <horusjr@me.com>
2024-09-02 07:52:57 -0700

    stack fully works

--- linkedin/python_dsa/stack.py
+++ linkedin/python_dsa/stack.py
@@ -5,7 +5,7 @@ class Stack:
     def __init__(self):
         super().__init__()
         self.items: list = []
-        self.top_idx: int = 0
+        self.top_idx: int = -1
 
     def push(self, item: any) -> None:
 
@@ -17,7 +17,7 @@ class Stack:
         self.top_idx += 1
 
     def pop(self) -> any:
-        if self.top_idx == 0:
+        if self.top_idx == -1:
             raise IndexError("Empty stack")
         else:
             top_node = self.items[self.top_idx]
@@ -25,14 +25,26 @@ class Stack:
             return top_node.data
 
     def peek(self) -> any:
-        if self.top_idx == 0:
+        if self.top_idx == -1:
             raise IndexError("Empty stack")
         else:
             top_node = self.items[self.top_idx]
             return top_node.data
 
     def is_empty(self) -> bool:
-        if self.top_idx == 0:
+        if self.top_idx == -1:
             return True
         else:
             return False
+
+    def __str__(self) -> str:
+        if self.is_empty():
+            return "[]"
+        else:
+            str_rep = ""
+            for i in range(0, self.top_idx + 1):
+                if i == 0:
+                    str_rep += str(self.items[i])
+                else:
+                    str_rep += f",{str(self.items[i])}"
+            return str_rep
--- linkedin/python_dsa/test_stack.py
+++ linkedin/python_dsa/test_stack.py
@@ -17,3 +17,34 @@ def test_std_library_stack_pop_empty():
     with pytest.raises(IndexError) as e:
         _ = node_stack.pop()
     assert str(e.value) == "pop from empty list"
+
+def test_push_1():
+    int_stack = Stack()
+    int_stack.push(1)
+    stack_str = str(int_stack)
+    expected = "[1]"
+    assert stack_str == expected
+
+def test_push_2():
+    int_stack = Stack()
+    int_stack.push(1)
+    int_stack.push(2)
+    stack_str = str(int_stack)
+    expected = "[1],[2]"
+    assert stack_str == expected
+
+def test_push_3_pop_1():
+    int_stack = Stack()
+    int_stack.push(1)
+    int_stack.push(2)
+    int_stack.push(3)
+    top = int_stack.pop()
+    expected = 3
+    assert top == expected
+
+    stack_str = str(int_stack)
+    expected = "[1],[2]"
+    assert stack_str == expected    
+
+if __name__ == "__main__":
+    test_push_3_pop_1()
