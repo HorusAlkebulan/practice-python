262201f06324d5e0f5c3a4f656f54326543308f5
Horus Alkebu-Lan <horusjr@me.com>
2024-09-05 11:42:25 -0700

    continued test work

---
+++ linkedin/open_source_projects_python/test_reminder.py
@@ -0,0 +1,57 @@
+# test_reminder.py
+
+import reminder as app
+from reminder import Task
+import pytest
+import datetime as dt
+
+@pytest.fixture
+def task_list() -> list:
+    return [
+        Task(name="pay rent"),
+        Task(name="buy bread"),
+    ]
+
+def test_find_task():
+    task_list = [
+        Task(name="pay rent"),
+        Task(name="buy bread"),
+    ]
+
+    expected = Task(name="buy bread")
+    assert app._find_task("buy bread", task_list) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("buy bread", Task(name="buy bread")),
+        ("buy banana", None),
+    ],
+)
+def test_find_task_with_params(test_input, expected):
+    task_list = [
+        Task(name="pay rent"),
+        Task(name="buy bread"),
+    ]
+    assert app._find_task(test_input, task_list) == expected
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("buy bread", Task(name="buy bread")),
+        ("buy banana", None),
+    ],
+)
+def test_find_task_with_fixture(test_input, expected, task_list):
+    assert app._find_task(test_input, task_list) == expected
+
+
+def test_to_date():
+    expected = dt.date(2022, 9, 1)
+    assert app._to_date("2022-09-01") == expected
+
+
+def test_to_date_with_exceptions():
+    with pytest.raises(ValueError) as e:
+        app._to_date("1234")
