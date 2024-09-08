0ce33385d44417d9ac247de93556e7e7ff747258
Horus Alkebu-Lan <horusjr@me.com>
2024-09-08 08:53:40 -0700

    another example tesg

--- linkedin/advanced_algo_thinking/test_knapsack.py
+++ linkedin/advanced_algo_thinking/test_knapsack.py
@@ -18,3 +18,17 @@ def test_knapsack_ex1():
     assert actual[1] == expected_row_1
     expected_row_4 = [0, 10, 10, 20, 30, 35, 45]
     assert actual[4] == expected_row_4
+
+
+def test_knapsack_ex2():
+    max_capacity = 6
+    weights = [0, 1, 3, 5, 6]
+    values = [0, 25, 30, 40, 35]
+
+    actual = knapsack(max_capacity, weights, values)
+    expected_row_0 = [0, 0, 0, 0, 0, 0, 0]
+    assert actual[0] == expected_row_0
+    expected_row_1 = [0, 25, 25, 25, 25, 25, 25]
+    assert actual[1] == expected_row_1
+    expected_row_4 = [0, 25, 25, 30, 55, 55, 65]
+    assert actual[4] == expected_row_4
