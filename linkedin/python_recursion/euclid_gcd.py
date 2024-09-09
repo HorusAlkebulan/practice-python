fe88c0b1f6662b6b622dc7881503539c7ce5b58e
Horus Alkebu-Lan <horusjr@me.com>
2024-09-09 09:42:43 -0700

    isort black

--- linkedin/python_recursion/euclid_gcd.py
+++ linkedin/python_recursion/euclid_gcd.py
@@ -1,13 +1,6 @@
 # euclid_gcd.py
 
 
-# def euclid_gcd(a_list: list):
-#     result = 0
-#     for val in a_list:
-#         result += val
-#     return result
-
-
 def euclid_gcd_recursive(a: int, b: int):
     # base case(s): empty, single value, etc.
     if b == 0:
